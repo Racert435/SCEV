dd120f2b55d6f0f5fe8fc9fa5a133f4c06f4f70d
Racert435 <hidas435@gmail.com>
2021-12-05 12:23:11 -0600

    first commit 7

--- src/index.js
+++ src/index.js
@@ -12,12 +12,12 @@ import Noencontrada from './routes/Noencontrada'
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element ={<App/>}>
+      <Route path='/' element ={<App/>}> </Route>
         <Route index element={<Inicio/>}></Route>
-        <Route path='formulario' element={<Formulario/>}></Route>
-        <Route path='quien' element={<Quien/>}></Route>
+        <Route path='/formulario' element={<Formulario/>}></Route>
+        <Route path='/quien' element={<Quien/>}></Route>
         <Route path='*' element={<Noencontrada/>}></Route>
-      </Route>
+     
     </Routes>
     
   </BrowserRouter>,
